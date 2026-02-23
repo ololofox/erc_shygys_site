fc8499098f6e344c83a52b9f93eb717170866301
ololofox <121015076+ololofox@users.noreply.github.com>
2026-02-23 20:10:54 +0500

    ip
    2026-23-02
    ip поменял

--- shygys-erc-site/src/store/mainStore.js
+++ shygys-erc-site/src/store/mainStore.js
@@ -2,8 +2,8 @@ import { defineStore } from 'pinia'
 
 export const mainStore = defineStore('mainStore', {
   state: () => ({
-    //routeAPI: 'http://10.250.180.144:8081/api/',
-    routeAPI: 'http://localhost:8081/api',
+    //routeAPI: 'http://localhost:8081/api/',
+    routeAPI: 'http://91.147.113.202:8081/api',
     APIkey: 'bbc2a9b4da6439c27edb00288c42e17e4519b682d0201fbe',
     accessToken: null,
     lastFeedbackTime: null,
